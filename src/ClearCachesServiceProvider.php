9aa0cd93f01c3a61ce37ba4f8624e6970868bcc5
boolfalse <boolfalse@mail.ru>
2018-09-26 11:56:24 +0300

    service provider bug fixed

--- src/ClearCachesServiceProvider.php
+++ src/ClearCachesServiceProvider.php
@@ -15,13 +15,7 @@ class ClearCachesServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/views', 'clearcaches'); // second param is package_name
-
-        // publishing the view
-        $this->publishes([
-            __DIR__ . '/views' => resource_path('views/vendor/clearcaches'),
-        ]);
+		//
     }
 
     public function register()
